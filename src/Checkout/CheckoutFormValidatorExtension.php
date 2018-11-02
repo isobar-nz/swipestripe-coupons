9dbc3503344dddad2878be8fddb0e4777a304a5a
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-11-02 13:50:45 +1300

    Validate already applied coupons at checkout
    Stops users from applying a valid coupon, then changing their order to
    use the coupon in a non-qualifying manner.

--- src/Checkout/CheckoutFormValidatorExtension.php
+++ src/Checkout/CheckoutFormValidatorExtension.php
@@ -5,8 +5,10 @@ namespace SwipeStripe\Coupons\Checkout;
 
 use SilverStripe\Core\Extension;
 use SwipeStripe\Coupons\Order\OrderCoupon;
+use SwipeStripe\Coupons\Order\OrderExtension;
 use SwipeStripe\Order\Checkout\CheckoutForm;
 use SwipeStripe\Order\Checkout\CheckoutFormValidator;
+use SwipeStripe\Order\Order;
 
 /**
  * Class CheckoutFormValidatorExtension
@@ -21,6 +23,8 @@ class CheckoutFormValidatorExtension extends Extension
      */
     public function validate(CheckoutForm $form, array $data): void
     {
+        $this->owner->validateAppliedCoupons($form->getCart());
+
         $code = $data[CheckoutFormExtension::COUPON_CODE_FIELD];
         if (empty($code)) {
             return;
@@ -34,6 +38,19 @@ class CheckoutFormValidatorExtension extends Extension
             $this->owner->getResult()->combineAnd(
                 $coupon->isValidFor($form->getCart(), CheckoutFormExtension::COUPON_CODE_FIELD)
             );
+        }
+    }
+
+    /**
+     * @param Order|OrderExtension $cart
+     */
+    public function validateAppliedCoupons(Order $cart): void
+    {
+        $couponAddOn = $cart->getCouponAddOn();
+
+        if ($couponAddOn->exists()) {
+            $validationResult = $couponAddOn->OrderCoupon()->isValidFor($cart, '');
+            $this->owner->getResult()->combineAnd($validationResult);
         }
     }
 }
